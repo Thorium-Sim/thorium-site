c4894fbdb9a0442d854f2ca9b90d374d719a8163
Alex Anderson <alexanderson1993@gmail.com>
2017-08-12 09:29:16 -0600

    Redirect to HTTPS

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import './style';
-import App from './components/app';
+import "./style";
+import App from "./components/app";
 
+if (location.protocol === "http:" && location.hostname !== "localhost") {
+  window.location = `https://${location.host}${location.pathname}`;
+}
 export default App;
